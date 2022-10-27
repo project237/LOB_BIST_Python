ebbbca24ab23549b4649b5ff0eb4d98f6002de6c
user237 <berkozer12@gmail.com>
2022-10-28 02:21:40 +0300

    Updated main.py so that it correctly runs the updated OrderEngine object.

--- main.py
+++ main.py
@@ -1,35 +1,45 @@
 
 # =================================== PARAMETERS ==================================
-INPUT_FILE_NAME = "GARAN.E.csv"
+INPUT_FILE_NAME = "GARAN.E.test.csv"
 
 # ==================================== IMPORTS ====================================
 from OrderEngine import OrderEngine
-from order import order
+# from order import order
 from pprint import pprint
 
 # ============================== METHODS & EXECUTION ==============================
 def process_order(line):
     pass
 
-def main():
+# def main0():
 
-    order_engine = OrderEngine()
+#     order_engine = OrderEngine()
+
+#     with open(INPUT_FILE_NAME, "r") as f:
+
+#         for i, l in enumerate(f):
+
+#             line = l.split(",")
+
+#             try:
+#                 new_order = order(*line)
+#                 order_engine.process_order(new_order)
+#             except ValueError:
+#                 print(f"Error: Invalid order at line {(i+1)}")
+        
+#     print()
+#     pprint(order_engine.all_orders)
+#     pprint(order_engine.active_orders)
+
+def main():
+    ord_engine = OrderEngine()
 
     with open(INPUT_FILE_NAME, "r") as f:
-
         for i, l in enumerate(f):
-
-            line = l.split(",")
-
             try:
-                new_order = order(*line)
-                order_engine.process_order(new_order)
-            except ValueError:
+                ord_engine.process_order(l)
+            except Exception:
                 print(f"Error: Invalid order at line {(i+1)}")
-        
-    print()
-    pprint(order_engine.all_orders)
-    pprint(order_engine.active_orders)
 
 if __name__ == '__main__':
     main()
